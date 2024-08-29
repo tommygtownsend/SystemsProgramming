d6265506bc260e37dfdcc6fa48ca1db69b78a3e5
tommygtownsend <101840423+tommygtownsend@users.noreply.github.com>
2024-08-29 20:59:56 +0000

    test push

--- Lab01.c
+++ Lab01.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+#include <math.h>
 
+int main(int argc, char* argv[])
+{
+    int given_number;
+    printf("Gimmie a number!");
+    scanf("%d",&given_number);
+    if (given_number <= 1)
+    printf("%d is prime");
+     
+    return 0;  
+
+}
