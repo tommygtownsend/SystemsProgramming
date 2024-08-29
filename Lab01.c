a6223734a078b725189e231ef3c48a99265a89ef
tommygtownsend <101840423+tommygtownsend@users.noreply.github.com>
2024-08-29 21:30:10 +0000

    new stuff

--- Lab01.c
+++ Lab01.c
@@ -2,13 +2,30 @@
 #include <math.h>
 
 int main(int argc, char* argv[])
-{
+{  
+    int i;
+    int primeflag = 0;
     int given_number;
-    printf("Gimmie a number!");
-    scanf("%d",&given_number);
-    if (given_number <= 1)
-    printf("%d is prime");
-    
-    return 0;  
+    printf("Give me a number: ");
+    scanf("%d", &given_number);
 
+    if (given_number <= 1) {
+        printf("%d is not prime.\n", given_number);
+        primeflag = 1; 
+    }
+    
+    for (i=2; i<=given_number/2; i++){
+        if(given_number % i == 0){
+            printf("%d is not prime.\n", given_number);
+            primeflag = 1;
+        }
+        if (given_number == 2){
+            primeflag = 0;
+        }
+    }
+    
+    if (primeflag == 0){
+        printf("%d IS Prime\n",given_number);
+    }
+    return 0;  
 }
