d2f0412efdfa1ea60984534d713b59eb2bcf206d
tommygtownsend <101840423+tommygtownsend@users.noreply.github.com>
2024-09-01 22:31:58 +0000

    final Lab01

--- Lab01.c
+++ Lab01.c
@@ -1,37 +1,44 @@
 #include <stdio.h>
 #include <math.h>
 
-int main(int argc, char* argv[])
+int main(int argc, char* argv[])//I kept the paramters the same as our hello world on Thursday.
+
 {  
+    int a = 1;
+    while (a){//like in fridays lecture, I used the good ole infinite while loop to keep the program running 
 
     //welcome to ye old variables shop. Feel free to peruse!
     int i;                          
-    int primeflag = 0;
+    int primeflag = 0;//in the court of law, all ints are assumed prime unless proven otherwise
     int given_number;
 
     //Printin'n'Scannin'
     printf("Give me a number: ");
     scanf("%d", &given_number); 
 
-    //if then section to determine if given_number is prime, with a for-loop
+    //logical section
     if (given_number <= 1) {
-        printf("%d is not prime.\n", given_number);
         primeflag = 1; 
     }
-    
+    //we check if any of the first half of possible divisors yeild no remainder. If so, raise the flag
     for (i=2; i<=given_number/2; i++){
         if(given_number % i == 0){
-            printf("%d is not prime.\n", given_number);
             primeflag = 1;
         }
-        if (given_number == 2){
+        if (given_number == 2){//2 is only prime divisible by 2 
             primeflag = 0;
         }
     }
-    
-    if (primeflag == 0){
-        printf("%d IS Prime\n",given_number);
+    //I used a switch case for the answer, like in the friday lecture. I like how clean it looks!
+    switch (primeflag) {
+        case 1:
+            printf("%d is NOT prime.\n", given_number);
+        break;
+        case 0: 
+            printf("%d IS Prime\n",given_number);
+        break;
+    }
     }
     //to kill the main function you return 0
-    return 0;  
+    return 0;//we'll never make it to this line, because of the while loop.      
 }
