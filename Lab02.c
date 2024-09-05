319ed6c48aff90eab35ae755f57c3df8005a10cf
tommygtownsend <101840423+tommygtownsend@users.noreply.github.com>
2024-09-05 22:35:06 +0000

    lab2 4

--- Lab02.c
+++ Lab02.c
@@ -19,25 +19,28 @@ int arr[n];
 
  int i, key, j;
     for (i = 1; i < n; i++) {
-        key = arr[i];
-        j = i - 1;
+        key = arr[i]; //key is the current value of array
+        j = i - 1; //j is the index before i
 
         // Move elements of arr[0..i-1] that are greater than key to one position ahead
-        while (j >= 0 && arr[j] > key) {
-            arr[j + 1] = arr[j];
-            j = j - 1;
+        while (j >= 0 && arr[j] > key) { //while j not negative and the key value is smaller than the value in j
+            arr[j + 1] = arr[j]; // replaces the array value at j with the array value at j+1
+            j = j - 1; //decrements j
         }
-        arr[j + 1] = key;
+        arr[j + 1] = key; //new key value
     }
 
- int i2;
-    for (i2 = 0; i2 < n; i2++) {
-        printf("%d ", arr[i2]);
-    }
-    printf("\n");
+
+
 
 
  printf("Sorted array:\n");
+ int i2;
+    for (i2 = 0; i2 < n; i2++) {
+        printf("%d ", arr[i2]);
+        printf("\n");
+    }
+    printf("\n");
 
 
 return 0;
