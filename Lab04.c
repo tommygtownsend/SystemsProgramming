4b5e763a1cf6029137d3b4027b45c65e43836bef
tommygtownsend <101840423+tommygtownsend@users.noreply.github.com>
2024-09-18 19:59:11 +0000

    save 2

--- Lab04.c
+++ Lab04.c
@@ -1,5 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 
-int main(){
+#define BUFFSIZE 4096
+int main(int argc, char *argv[]) {
+int readFileDescriptor, writeFileDescriptor;
+long int n;
+char buf[BUFFSIZE];
+if (argc != 3){
+printf("Usage: %s <source_filename> <destination_filename>\n",
+argv[0]);
+exit (-1);
+}
+readFileDescriptor = open(argv[1], O_RDONLY);
+writeFileDescriptor = open(argv[2], O_CREAT|O_WRONLY|O_TRUNC, 0700);
+
+
+if (readFileDescriptor == -1 || writeFileDescriptor == -1){
+printf("Error with file open\n");
+exit (-1);
+}
+
+
+
+
+
+
+
+
 
 }
