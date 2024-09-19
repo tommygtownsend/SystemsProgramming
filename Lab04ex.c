b07a8763f746bc0bb1f7a0a72e4632bb81c568b1
tommygtownsend <101840423+tommygtownsend@users.noreply.github.com>
2024-09-19 19:51:21 +0000

    h

--- Lab04ex.c
+++ Lab04ex.c
@@ -5,19 +5,20 @@
 
 #define BUFFSIZE 4096
 
-
 int main(int argc, char *argv[]) {
 int readFileDescriptor, writeFileDescriptor;
 long int n;
 char buf[BUFFSIZE];
 
+
 if (argc != 3){
-printf("Usage: %s <source_filename> <destination_filename>\n",
-argv[0]);
-exit (-1);
+    printf("Usage: %s <source_filename> <destination_filename>\n",
+    argv[0]);
+   exit (-1);
 }
-readFileDescriptor = open(argv[1], O_RDONLY);
-writeFileDescriptor = open(argv[2], O_CREAT|O_WRONLY|O_TRUNC, 0700);
+
+readFileDescriptor = open(argv[2], O_RDONLY);
+writeFileDescriptor = open(argv[1], O_CREAT|O_WRONLY|O_TRUNC, 0700);
 
 /*
 Use the open function in read only mode to read the input file. The open function
@@ -31,19 +32,22 @@ of the new file created for writing. Note that the UNIX file uses {read, write, 
 supported by the open function from the man page or the text book.
 */
 if (readFileDescriptor == -1 || writeFileDescriptor == -1){
-printf("Error with file open\n");
-exit (-1);
+    printf("Error with file open\n");
+    exit (-1);
 }
 
 while ((n = read(readFileDescriptor, buf, BUFFSIZE)) > 0){
-if (write(writeFileDescriptor, buf, n) != n){
-printf("Error writing to output file\n");
-exit (-1);
+
+
+    if (write(writeFileDescriptor, buf, n) != n){
+        printf("Error writing to output file\n");
+        exit (-1);
+    }
 }
-}
+
 if (n < 0){
-printf("Error reading input file\n");
-exit (-1);
+    printf("Error reading input file\n");
+    exit (-1);
 }
 
 
