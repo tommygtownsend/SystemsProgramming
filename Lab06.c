5bb214a4e4f93be84dde9c9f1e861d9f53df99ed
tommygtownsend <101840423+tommygtownsend@users.noreply.github.com>
2024-10-06 19:30:50 +0000

    test

--- Lab06.c
+++ Lab06.c
@@ -6,7 +6,21 @@
 #define LINESIZE 1024
 #define MAX_ITEMS 1000
 
-
+struct listing {
+    int id;
+    int host_id;
+    char *host_name;
+    char *neighbourhood_group;
+    char *neighbourhood;
+    float latitude;
+    float longitude;
+    char *room_type;
+    float price;
+    int minimum_nights;
+    int number_of_reviews;
+    int calculated_host_listings_count;
+    int availability_365;
+};
 
 
 int main(){
