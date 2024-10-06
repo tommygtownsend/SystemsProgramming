05b25a86bf8236d9b62d68e2a156ef24083bd809
tommygtownsend <101840423+tommygtownsend@users.noreply.github.com>
2024-10-06 19:38:33 +0000

    test

--- Lab06.c
+++ Lab06.c
@@ -87,7 +87,12 @@ int compareByHostName(const void *a, const void *b) {
     return strcmp(((struct listing*)a)->host_name, ((struct listing*)b)->host_name);
 }
 
-int compareByPrice(const void *a, const void *b)
+int compareByPrice(const void *a, const void *b){
+    float priceA = ((struct listing*)a)->price;
+    float priceB = ((struct listing*)b)->price;
+    return (priceA > priceB) - (priceA < priceB);
+}
+
 
 
 int main(){
