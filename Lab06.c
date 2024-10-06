a4b86bad3b6d60e53ef4f0e914f55551cedb823d
tommygtownsend <101840423+tommygtownsend@users.noreply.github.com>
2024-10-06 19:31:53 +0000

    test

--- Lab06.c
+++ Lab06.c
@@ -20,7 +20,56 @@ struct listing {
     int number_of_reviews;
     int calculated_host_listings_count;
     int availability_365;
-};
+}
+
+
+
+
+struct listing getfields(char* line) {
+    struct listing item;
+    char *token;
+
+    token = strtok(line, ",");
+    item.id = atoi(token);
+    
+    token = strtok(NULL, ",");
+    item.host_id = atoi(token);
+    
+    token = strtok(NULL, ",");
+    item.host_name = strdup(token);
+    
+    token = strtok(NULL, ",");
+    item.neighbourhood_group = strdup(token);
+    
+    token = strtok(NULL, ",");
+    item.neighbourhood = strdup(token);
+    
+    token = strtok(NULL, ",");
+    item.latitude = atof(token);
+    
+    token = strtok(NULL, ",");
+    item.longitude = atof(token);
+    
+    token = strtok(NULL, ",");
+    item.room_type = strdup(token);
+    
+    token = strtok(NULL, ",");
+    item.price = atof(token);
+    
+    token = strtok(NULL, ",");
+    item.minimum_nights = atoi(token);
+    
+    token = strtok(NULL, ",");
+    item.number_of_reviews = atoi(token);
+    
+    token = strtok(NULL, ",");
+    item.calculated_host_listings_count = atoi(token);
+    
+    token = strtok(NULL, ",");
+    item.availability_365 = atoi(token);
+
+    return item;
+}
 
 
 int main(){
